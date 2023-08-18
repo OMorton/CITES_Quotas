865c372056f9dab6de1ff94e542b09a04cb166e7
Oscar Morton <oscarmorton94@gmail.com>
2023-08-18 16:03:35 +0100

    output format updated (inc contrasts)

--- 2_Analysis/2.2_Trait_Analysis.R
+++ 2_Analysis/2.2_Trait_Analysis.R
@@ -10,6 +10,7 @@ library(ggpubr)
 library(ggtext)
 library(viridis)
 library(brms)
+library(tidybayes)
 
 Quota_trade_listing <- read.csv("Data/CITES/Quotas/Rept_Quota_trade_listing.csv")
 All_Listed_IUCN <- read.csv("Data/IUCN/ALL_REPT_ASSESSMENTS_series.csv")  
@@ -273,6 +274,7 @@ bm_capt_plt <- ggplot(filter(bm_sum, Coarse_source == "Captive"), aes(bm_kg, .ep
   #coord_cartesian(ylim = c(0, 2200)) +
   scale_x_log10(breaks = c(0.01, 0.1, 1, 10, 100),
                 labels = c(0.01, 0.1, 1, 10, 100)) +
+  scale_y_log10() +
   theme_minimal()
 
 bm_wild_plt <- ggplot(filter(bm_sum, Coarse_source == "Wild"), aes(bm_kg, .epred)) +
@@ -376,46 +378,108 @@ final_trait_plt <- trait_plt +
 ggsave(path = "Outputs/Figures", final_trait_plt, filename = "Trait_plt.png",  bg = "white",
        device = "png", width = 25, height = 23, units = "cm")
 
+
+## bm
 bm_coef_sum <- fixef(bm_mod, summary = FALSE) %>% as.data.frame() %>%
+  mutate(log_bm_captive = log_bm_z,
+         log_bm_wild = log_bm_z + `log_bm_z:Coarse_sourceWild`,
+         log_bm_diff_wild = `log_bm_z:Coarse_sourceWild`) %>%
+  select(log_bm_captive, log_bm_wild, log_bm_diff_wild) %>%
   pivot_longer(everything(), names_to = "coef", values_to = "val") %>%
   group_by(coef) %>%
-  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100)) %>%
-  group_by(coef, pd) %>%
+  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100),
+         val = val/sd(bm_dat$log_bm),
+         type = "sd_raw",
+         trait = "bm") %>%
+  group_by(coef, pd, type, trait) %>%
   median_hdci(val, .width = .9)
 
+
+## ml
 ml_coef_sum <- fixef(ml_mod, summary = FALSE) %>% as.data.frame() %>%
+  mutate(logml_captive = log_ml_z,
+         logml_wild = log_ml_z + `log_ml_z:Coarse_sourceWild`,
+         logml_diff_wild = `log_ml_z:Coarse_sourceWild`) %>%
+  select(logml_captive, logml_wild, logml_diff_wild) %>%
   pivot_longer(everything(), names_to = "coef", values_to = "val") %>%
   group_by(coef) %>%
-  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100)) %>%
-  group_by(coef, pd) %>%
+  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100),
+         val = val/sd(ml_dat$log_ml),
+         type = "sd_raw",
+         trait = "ml") %>%
+  group_by(coef, pd, type, trait) %>%
   median_hdci(val, .width = .9)
 
+## cl
 cl_coef_sum <- fixef(cl_mod, summary = FALSE) %>% as.data.frame() %>%
+  mutate(cl_captive = cl_z,
+         cl_wild = cl_z + `cl_z:Coarse_sourceWild`,
+         cl_diff_wild = `cl_z:Coarse_sourceWild`) %>%
+  select(cl_captive, cl_wild, cl_diff_wild) %>%
   pivot_longer(everything(), names_to = "coef", values_to = "val") %>%
   group_by(coef) %>%
-  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100)) %>%
-  group_by(coef, pd) %>%
+  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100),
+         val = val/sd(cl_dat$Litter_clutch_size),
+         type = "sd_raw",
+         trait = "cl") %>%
+  group_by(coef, pd, type, trait) %>%
   median_hdci(val, .width = .9)
 
+## hb
 hb_coef_sum <- fixef(hb_mod, summary = FALSE) %>% as.data.frame() %>%
+  mutate(hb_captive = hb_z,
+         hb_wild = hb_z + `hb_z:Coarse_sourceWild`,
+         hb_diff_wild = `hb_z:Coarse_sourceWild`) %>%
+  select(hb_captive, hb_wild, hb_diff_wild) %>%
   pivot_longer(everything(), names_to = "coef", values_to = "val") %>%
   group_by(coef) %>%
-  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100)) %>%
-  group_by(coef, pd) %>%
+  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100),
+         val = val/sd(hb_dat$hab_breadth),
+         type = "sd_raw",
+         trait = "hb") %>%
+  group_by(coef, pd, type, trait) %>%
   median_hdci(val, .width = .9)
 
+## iucn
 iucn_coef_sum <- fixef(iucn_mod, summary = FALSE) %>% as.data.frame() %>%
   pivot_longer(everything(), names_to = "coef", values_to = "val") %>%
   group_by(coef) %>%
-  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100)) %>%
-  group_by(coef, pd) %>%
+  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100),
+         type = "sd_raw",
+         trait = "iucn") %>%
+  group_by(coef, pd, type, trait) %>%
   median_hdci(val, .width = .9)
 
-write.csv(bm_coef_sum, "Outputs/Summary/F4/bm_coef_sum.csv")
+iucn_contrast_sum <- fixef(iucn_mod, summary = FALSE) %>% as.data.frame() %>%
+  mutate(LC = exp(Intercept + Coarse_sourceWild),
+         NT = exp(Intercept + Coarse_sourceWild + IUCN_codeNT + `IUCN_codeNT:Coarse_sourceWild`),
+         VU = exp(Intercept + Coarse_sourceWild + IUCN_codeVU +`IUCN_codeVU:Coarse_sourceWild`),
+         EN = exp(Intercept + Coarse_sourceWild + IUCN_codeEN +`IUCN_codeEN:Coarse_sourceWild`),
+         CR = exp(Intercept + Coarse_sourceWild + IUCN_codeCR +`IUCN_codeCR:Coarse_sourceWild`),
+         NE = exp(Intercept + Coarse_sourceWild + IUCN_codeNE +`IUCN_codeNE:Coarse_sourceWild`),
+         LC_VU = LC - VU,
+         LC_EN = LC - EN,
+         LC_CR = LC - CR,
+         NT_VU = NT - VU,
+         NT_EN = NT - EN,
+         NT_CR = NT - CR) %>%
+  select(LC_VU, LC_EN, LC_CR, NT_VU, NT_EN, NT_CR) %>%
+  pivot_longer(everything(), names_to = "contr", values_to = "val") %>%
+  group_by(contr) %>%
+  mutate(pd = (sum(sign(val) == sign(median(val)))/n()*100)) %>%
+  group_by(contr, pd) %>%
+  median_hdci(val, .width = .9)
+  
+
+
+write.csv(bm_coef_sum, "Outputs/Summary/F4/trait_coef_sum.csv")
 write.csv(cl_coef_sum, "Outputs/Summary/F4/cl_coef_sum.csv")
 write.csv(ml_coef_sum, "Outputs/Summary/F4/ml_coef_sum.csv")
 write.csv(hb_coef_sum, "Outputs/Summary/F4/hb_coef_sum.csv")
 write.csv(iucn_coef_sum, "Outputs/Summary/F4/iucn_coef_sum.csv")
+write.csv(rbind(bm_coef_sum, cl_coef_sum, ml_coef_sum, hb_coef_sum, iucn_coef_sum),
+          "Outputs/Summary/F4/ALL_coef_sum.csv")
+
 
 write.csv(bm_dat, "Outputs/Summary/F4/bm_fitting_dat.csv")
 write.csv(cl_dat, "Outputs/Summary/F4/cl_fitting_dat.csv")
