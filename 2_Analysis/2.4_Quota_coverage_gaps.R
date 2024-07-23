6c32b5ac0cc95a63cb13300400726a15b9aa9d68
OMorton <oscarmorton94@gmail.com>
2024-07-23 16:27:21 +0100

    resized figures

--- 2_Analysis/2.4_Quota_coverage_gaps.R
+++ 2_Analysis/2.4_Quota_coverage_gaps.R
@@ -37,25 +37,25 @@ trade_quota_genjoin %>% filter(is.na(n_quotas), Int_trade_thr_L %in% c("Yes"), Y
 
 Int_line_plt <- ggplot(time_sum_int, aes(Year)) +
   geom_line(aes(y = n), colour = "darkred", size = 1) +
-  geom_line(aes(y = vol/2000), colour = "darkred", alpha = .5, linetype = "dashed", size = 1) +
+  geom_line(aes(y = vol/2000), colour = "darkred", alpha = .5, linetype = "dashed", size = .5) +
   scale_y_continuous(name = "Richness", 
                      sec.axis = sec_axis(~.*2000, name="Volume")) +
-  theme_minimal()
+  theme_minimal(base_size = 8)
 
 Thr_line_plt <- ggplot(time_sum_thr, aes(Year)) +
   geom_line(aes(y = n), colour = "darkorange", size = 1) +
-  geom_line(aes(y = vol/2000), colour = "darkorange", alpha = .5, linetype = "dashed", size = 1) +
+  geom_line(aes(y = vol/2000), colour = "darkorange", alpha = .5, linetype = "dashed", size = .5) +
   scale_y_continuous(name = "Richness", 
                      sec.axis = sec_axis(~.*2000, name="Volume")) +
   coord_cartesian(ylim = c(0, 35)) +
-  theme_minimal()
+  theme_minimal(base_size = 8)
 
 DD_line_plt <- ggplot(time_sum_NE, aes(Year)) +
   geom_line(aes(y = n), colour = "darkblue", size = 1) +
-  geom_line(aes(y = vol/2000), colour = "darkblue", alpha = .5, linetype = "dashed", size = 1) +
+  geom_line(aes(y = vol/2000), colour = "darkblue", alpha = .5, linetype = "dashed", size = .5) +
   scale_y_continuous(name = "Richness", 
                      sec.axis = sec_axis(~.*2000, name="Volume")) +
-  theme_minimal()
+  theme_minimal(base_size = 8)
 
 
 library(rnaturalearth)
@@ -106,16 +106,18 @@ DD_hist_plt <- ggplot() +
   coord_sf(ylim = c(-50, 90), datum = NA) +
   scale_fill_steps(low = "skyblue", high = "darkblue", na.value="grey90",
                    limits = c(1, 80)) +
-  theme_classic(base_size = 14) +
-  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank())
+  theme_classic(base_size = 10) +
+  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank(),
+        legend.key.size = unit(0.4, "cm"))
 
 DD_rec_plt <- ggplot() + 
   geom_sf(data = time_sp_NE_rec, aes(geometry = geometry, fill = n), colour = NA, size = .01) +
   coord_sf(ylim = c(-50, 90), datum = NA) +
   scale_fill_steps(low = "skyblue", high = "darkblue", na.value="grey90",
                    limits = c(1, 80)) +
-  theme_classic(base_size = 14) +
-  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank())
+  theme_classic(base_size = 10) +
+  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank(),
+        legend.key.size = unit(0.4, "cm"))
 
 ## Thr
 Thr_hist_plt <- ggplot() + 
@@ -123,16 +125,18 @@ Thr_hist_plt <- ggplot() +
   coord_sf(ylim = c(-50, 90), datum = NA) +
   scale_fill_steps(low = "lightgoldenrod1", high = "darkorange", na.value="grey90",
                    limits = c(1, 66)) +
-  theme_classic(base_size = 14) +
-  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank())
+  theme_classic(base_size = 10) +
+  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank(),
+        legend.key.size = unit(0.4, "cm"))
 
 Thr_rec_plt <- ggplot() + 
   geom_sf(data = time_sp_thr_rec, aes(geometry = geometry, fill = n), colour = NA, size = .01) +
   coord_sf(ylim = c(-50, 90), datum = NA) +
   scale_fill_steps(low = "lightgoldenrod1", high = "darkorange", na.value="grey90",
                    limits = c(1, 66)) +
-  theme_classic(base_size = 14) +
-  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank())
+  theme_classic(base_size = 10) +
+  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank(),
+        legend.key.size = unit(0.4, "cm"))
 
 ## L Thr
 Int_hist_plt <- ggplot() + 
@@ -140,16 +144,18 @@ Int_hist_plt <- ggplot() +
   coord_sf(ylim = c(-50, 90), datum = NA) +
   scale_fill_steps(low = "lightgoldenrod1", high = "darkred", na.value="grey90",
                    limits = c(1, 42)) +
-  theme_classic(base_size = 14) +
-  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank())
+  theme_classic(base_size = 10) +
+  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank(),
+        legend.key.size = unit(0.4, "cm"))
 
 Int_rec_plt <- ggplot() + 
   geom_sf(data = time_sp_int_rec, aes(geometry = geometry, fill = n), colour = NA, size = .01) +
   coord_sf(ylim = c(-50, 90), datum = NA) +
   scale_fill_steps(low = "lightgoldenrod1", high = "darkred", na.value="grey90",
                    limits = c(1, 42)) +
-  theme_classic(base_size = 14) +
-  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank())
+  theme_classic(base_size = 10) +
+  theme(panel.grid = element_blank(), legend.position = "right", legend.title = element_blank(),
+        legend.key.size = unit(0.4, "cm"))
 
 library(ggpubr)
 
@@ -165,22 +171,24 @@ coverage_plt_alt <- ggarrange(
   nrow = 3)
 
 coverage_plt_alt2 <- coverage_plt_alt + 
-  annotation_custom(text_grob("Historical (1997 - 2015)", face = "bold", size= 14, hjust = 0), 
+  annotation_custom(text_grob("Historical (1997 - 2015)", face = "bold", size= 9, hjust = 0), 
                     xmin = 0.37, xmax = 0.37, ymin = 0.97, ymax = 0.97) +
-  annotation_custom(text_grob("Recent (2016 - 2021)", hjust = 0,face = "bold", size= 14), 
+  annotation_custom(text_grob("Recent (2016 - 2021)", hjust = 0,face = "bold", size= 9), 
                     xmin = 0.68, xmax = 0.68, ymin = 0.97, ymax = 0.97) +
-  annotation_custom(text_grob("80 exporters, 248 taxa", hjust = 0,face = "italic", size= 10), 
+  annotation_custom(text_grob("80 exporters, 248 taxa", hjust = 0,face = "italic", size= 7), 
                     xmin = 0.37, xmax = 0.37, ymin = 0.67, ymax = 0.67) +
-  annotation_custom(text_grob("19 exporters, 27 taxa", hjust = 0,face = "italic", size= 10), 
+  annotation_custom(text_grob("19 exporters, 27 taxa", hjust = 0,face = "italic", size= 7), 
                     xmin = 0.68, xmax = 0.68, ymin = 0.67, ymax = 0.67) +
-  annotation_custom(text_grob("64 exporters, 54 taxa", hjust = 0,face = "italic", size= 10), 
+  annotation_custom(text_grob("64 exporters, 54 taxa", hjust = 0,face = "italic", size= 7), 
                     xmin = 0.37, xmax = 0.37, ymin = 0.34, ymax = 0.34) +
-  annotation_custom(text_grob("28 exporters, 27 taxa", hjust = 0,face = "italic", size= 10), 
+  annotation_custom(text_grob("28 exporters, 27 taxa", hjust = 0,face = "italic", size= 7), 
                     xmin = 0.68, xmax = 0.68, ymin = 0.34, ymax = 0.34) +
-  annotation_custom(text_grob("41 exporters, 43 taxa", hjust = 0,face = "italic", size= 10), 
+  annotation_custom(text_grob("41 exporters, 43 taxa", hjust = 0,face = "italic", size= 7), 
                     xmin = 0.37, xmax = 0.37, ymin = 0.01, ymax = 0.01) +
-  annotation_custom(text_grob("8 exporters, 11 taxa", hjust = 0,face = "italic", size= 10), 
+  annotation_custom(text_grob("8 exporters, 11 taxa", hjust = 0,face = "italic", size= 7), 
                     xmin = 0.68, xmax = 0.68, ymin = 0.01, ymax = 0.01)
 
-ggsave(path = "Outputs/Figures", coverage_plt_alt2, filename = "coverage_alt_R1.png",  bg = "white",
-       device = "png", width = 26, height = 12, units = "cm")
+ggsave(path = "Outputs/FINAL_FIGURES", coverage_plt_alt2, filename = "Figure4.png",  bg = "white",
+       device = "png", width = 17, height = 8, units = "cm", dpi = 600)
+ggsave(path = "Outputs/FINAL_FIGURES", coverage_plt_alt2, filename = "Figure4.pdf",  bg = "white",
+       device = "pdf", width = 17, height = 8, units = "cm", dpi = 600)
